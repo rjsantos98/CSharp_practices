2d378bea5f4d814e34d1970bb6d51c570c6d255a
Raimundo Santos <rjsmatos98@gmail.com>
2019-09-22 05:05:40 -0300

    only commit

---
+++ Recursivas/NúmerosPrimos/Primos/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número positivo: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            SequenciaDePrimos(1,numero);
+
+            Console.ReadKey();
+        }
+
+        static bool NumeroPrimo(int numero, int contagem)
+        {
+            if(numero< 2)
+            {
+                return false;
+            }
+            if (contagem == numero)
+            {
+                return true;
+            }
+            else
+            {
+                if ((numero % contagem) == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return NumeroPrimo(numero,contagem+1);
+                }
+            }
+        }
+
+        static void SequenciaDePrimos(int contagem, int numero)
+        {
+            if(NumeroPrimo(contagem, 2))
+            {
+                Console.WriteLine("Ó número '"+contagem+"' é Primo");
+            }
+            if (contagem < numero)
+            {
+                SequenciaDePrimos(contagem + 1, numero);
+            }
+        }
+    }
+}
---
+++ Recursivas/PrimosRecursiva2/Primos2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimoOuNao(0, 2);
+            void PrimoOuNao(int numero, int contador)
+            {
+                if (numero == 0)
+                {
+                    Console.Write("Digite um número: ");
+                    numero = int.Parse(Console.ReadLine());
+                }
+                if (numero < 2 || numero == 1 && contador == numero)
+                {
+                    Console.WriteLine(numero + " não é primo");
+                }
+                else if (contador == numero)
+                {
+                    Console.WriteLine(numero + " é primo");
+                    numero = 0;
+                    PrimoOuNao(numero, contador = 2);
+                }
+                else
+                {
+                    if ((numero % contador) == 0)
+                    {
+                        Console.WriteLine(numero + " não é primo");
+                    }
+                    else
+                    {
+                        PrimoOuNao(numero, contador + 1);
+                    }
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
